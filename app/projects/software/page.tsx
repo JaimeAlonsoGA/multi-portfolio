9360592d1a17767c7b2319b3dc19c7dad7b3dafd
unknown <alonsog.jaime@gmail.com>
2025-02-05 23:11:41 +0100

    software projects

--- app/projects/software/page.tsx
+++ app/projects/software/page.tsx
@@ -1,30 +1,136 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Tag from "@/components/ui/tag";
+import { link } from "fs";
 import Link from "next/link";
+import { PiGooglePlayLogoLight } from "react-icons/pi";
 import { FaGithub } from "react-icons/fa6";
+import { TbWorld } from "react-icons/tb";
+
 
 
 export default function Software() {
     return (
-        <section>
-            <Card className="flex flex-col max-w-sm">
-                <CardHeader>
-                    <CardTitle>CSV to Excel</CardTitle>
-                    <CardDescription>Node program that converts a given csv into an Excel table</CardDescription>
-                </CardHeader>
-                <CardContent className="flex flex-wrap text-justify text-balance text-sm">
-                    <Tag label="node" />
-                    <Tag label="javascript" />
-                    <Tag label="excel" />
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <span className="flex flex-row gap-1 group">
-                        <a href="" className="text-gray-500 text-xs group-hover:underline">Github</a>
-                        <FaGithub />
-                    </span>
-                    <Link href={`projects/id`} className="text-xs text-gray-500 font-semibold">. . .</Link>
-                </CardFooter>
-            </Card>
+        <section className="w-full flex flex-wrap gap-4 max-h-screen overflow-y-auto scrollbar-hide">
+            {projects.map((project, i) => {
+                return (
+                    <Card key={i} className="flex flex-col gap-2">
+                        <CardHeader>
+                            <CardTitle>{project.title}</CardTitle>
+                            <CardDescription>{project.description}</CardDescription>
+                        </CardHeader>
+                        <CardContent className="flex flex-wrap text-justify text-balance text-sm">
+                            {project.tags.map((tag, i) => <Tag key={i} label={tag} />)}
+                        </CardContent>
+                        <CardFooter className="flex justify-between">
+                            <div className="flex flex-row gap-4 items-center">
+                                {project.github && (
+                                    <a href={project.github} target="_blank" className="flex flex-row gap-1 group items-center">
+                                        <p className="text-gray-500 text-xs group-hover:underline">Github</p>
+                                        <FaGithub />
+                                    </a>
+                                )}
+                                {project.link && (
+                                    <a href={project.link.url} target="_blank" className="flex flex-row gap-1 group items-center">
+                                        <p className="text-gray-500 text-xs group-hover:underline">Website</p >
+                                        <p className="text-xs text-gray-500 font-semibold">{project.link.icon}</p >
+                                    </a>
+                                )}
+                            </div>
+                            {project.seeMore && (
+                                <Link href={`projects/${project.title}`} className="text-xs text-gray-500 font-semibold">. . .</Link>
+                            )}
+                        </CardFooter>
+                    </Card>
+                )
+            })}
         </section>
     )
 }
+
+interface projects {
+    title: string;
+    description: string;
+    tags: string[];
+    github?: string;
+    link?: {
+        url: string;
+        icon: React.ReactNode;
+    }
+    seeMore?: boolean;
+}
+
+const appIcon = <PiGooglePlayLogoLight size={16} />
+const webIcon = <TbWorld size={16} />
+
+const projects = [
+    {
+        title: "Mirto Internship",
+        description: "Designed and developed end-to-end software solutions for MIRTO Group during my 9-month internship",
+        tags: ["react", "javascript", "typescript", "node", "graphql", "express", "tailwindcss"],
+        seeMore: true,
+    },
+    {
+        title: "Alchimix",
+        description: "Mobile app designed and developed to create custom cocktails with no limits",
+        tags: ["react native", "javascript", "expo", "figma"],
+        github: "https://github.com/JaimeAlonsoGA/alchimix",
+        link: {
+            url: "https://play.google.com/store/apps/details?id=com.sonicode.coctelapp&pcampaignid=web_share",
+            icon: appIcon
+        },
+        seeMore: true,
+    },
+    {
+        title: "Visu? Visu!",
+        description: "Mobile app designed and developed hand-by-hand with a biologist to help study the visu exam",
+        tags: ["react native", "javascript", "expo", "figma"],
+        github: "https://github.com/JaimeAlonsoGA/bio-visu",
+        seeMore: true,
+    },
+    {
+        title: "Vivilife Landing Page",
+        description: "Landing page developed for Vivilife, an app for coaches and communities",
+        tags: ["react", "tailwindcss"],
+        github: "https://github.com/JaimeAlonsoGA/vivilife_web",
+        link: {
+            url: "https://vivilife-web.web.app/",
+            icon: webIcon
+        }
+    },
+    {
+        title: "This website!",
+        description: "Yes, I wanted to make my own website from scratch",
+        tags: ["typescript", "nextjs", "tailwindcss", "vercel"],
+        github: "https://github.com/JaimeAlonsoGA/multi-portfolio",
+    },
+    {
+        title: "Musickeyboard.io",
+        description: "My attempt to make a web app to play music with your keyboard",
+        tags: ["react", "javascript", "tailwindcss"],
+        github: "https://github.com/JaimeAlonsoGA/musickeyboard_web",
+        link: {
+            url: "https://musickeyboard.web.app/",
+            icon: webIcon
+        }
+    },
+    {
+        title: "Jaime360",
+        description: "My old deprecated portfolio, also made from scratch",
+        tags: ["react", "javascript", "tailwindcss", "firebase"],
+        github: "https://github.com/JaimeAlonsoGA/portfolio",
+        link: {
+            url: "https://jaime360.web.app/",
+            icon: webIcon
+        }
+    },
+    {
+        title: "El Boske, mixing studio",
+        description: "Website designed and developed for a music studio in Madrid",
+        tags: ["react", "javascript", "tailwindcss", "firebase"],
+        github: "https://github.com/JaimeAlonsoGA/studio_web",
+        link: {
+            url: "https://elboskestudio.com/",
+            icon: webIcon
+        }
+    }
+]
--- components/featured.tsx
+++ components/featured.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/
 const Featured: React.FC = () => {
     return (
         <div>
-            <h2 className="text-sm font-[family-name:var(--font-geist-mono)] mb-4 text-center">2025's Featured</h2>
+            <h2 className="text-sm font-[family-name:var(--font-geist-mono)] mb-4 text-center text-gray-500 underline underline-offset-2">2025 Featured</h2>
             <div className="w-full flex flex-row justify-between gap-4">
                 <Card className="flex flex-col w-full">
                     <CardHeader>
@@ -19,7 +19,7 @@ const Featured: React.FC = () => {
                         <CardTitle>Interactive Media Demo Reel</CardTitle>
                         <CardDescription>Topics covered in this video are: sound design, music production and audio implementation into interactive media</CardDescription>
                     </CardHeader>
-                    <CardContent className="text-justify text-balance text-sm">
+                    <CardContent className="text-justify text-balance texFt-sm">
                         <iframe className="rounded-xl max-w-sm" width="560" height="315" src="https://www.youtube-nocookie.com/embed/uMCGGwNkLDA?si=L-WBtEcXoN3UnH7o&amp;controls=0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                     </CardContent>
                 </Card>
